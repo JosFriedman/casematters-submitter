34097d40e36f2d7b93e6bedd5caa498ef3bb60f3
Joseph Friedman <josfriedman@doitt.nyc.gov>
2019-10-04 11:55:38 -0400

    more

--- setup/db/lm/DEV/200_formSubmissionTrigger.sql
+++ setup/db/lm/DEV/200_formSubmissionTrigger.sql
@@ -8,24 +8,29 @@ GO
 
 
 CREATE TRIGGER [dbo].[formSubmissionTrigger]
-
    ON  [dbo].[Submissions]
-
    AFTER UPDATE
-
 AS
-
-
 BEGIN
     IF @@ROWCOUNT = 0
         return
 
     SET NOCOUNT ON
-    
-    EXEC CSC_CaseMatters_App_Dev.dbo.WebIntake_sp;
-    EXEC OATH_CaseMatters_App_Dev.dbo.WebIntake_sp;
-    EXEC OCB_CaseMatters_App_Dev.dbo.WebIntake_sp;
+
+    -- only call the WebIntake_sp procs if there are no attachments; if attachments, then the attachment trigger will call them
+	declare @submissionID int;
+    declare @attachmentCount int;
    
+    select @submissionID = submissionID from inserted;
+   	select @attachmentCount = count(submissionID) from SubmissionAttachments where submissionID = @submissionID;
+   
+	if (@attachmentCount = 0)
+	begin
+	    EXEC CSC_CaseMatters_App_Dev.dbo.WebIntake_sp;
+	    EXEC OATH_CaseMatters_App_Dev.dbo.WebIntake_sp;
+	    EXEC OCB_CaseMatters_App_Dev.dbo.WebIntake_sp;
+	end
 END
+
 
 
