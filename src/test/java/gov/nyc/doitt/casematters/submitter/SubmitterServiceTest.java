622f4cc8bcb593af3975d64fa00689a9245fbb9d
Joseph Friedman <josfriedman@doitt.nyc.gov>
2020-01-30 19:08:43 -0500

    more

--- src/test/java/gov/nyc/doitt/casematters/submitter/SubmitterServiceTest.java
+++ src/test/java/gov/nyc/doitt/casematters/submitter/SubmitterServiceTest.java
@@ -1,8 +1,18 @@
 package gov.nyc.doitt.casematters.submitter;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +20,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import gov.nyc.doitt.casematters.submitter.cmii.CmiiSubmissionService;
+import gov.nyc.doitt.casematters.submitter.cmii.model.CmiiSubmission;
 import gov.nyc.doitt.casematters.submitter.cmii.model.CmiiSubmissionMockerUpper;
 import gov.nyc.doitt.casematters.submitter.lm.LmSubmissionService;
+import gov.nyc.doitt.casematters.submitter.lm.model.LmSubmission;
 import gov.nyc.doitt.casematters.submitter.lm.model.LmSubmissionMockerUpper;
 import gov.nyc.doitt.casematters.submitter.task.JobStateManagerAccessor;
+import gov.nyc.doitt.casematters.submitter.task.TaskDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,7 +39,7 @@ public class SubmitterServiceTest extends TestBase {
 	private LmSubmissionMockerUpper lmSubmissionMockerUpper;
 
 	@Autowired
-	private JobDtoMockerUpper jobDtoMockerUpper;
+	private TaskDtoMockerUpper taskDtoMockerUpper;
 
 	@InjectMocks
 	private SubmitterService submitterService = new SubmitterService();
@@ -49,35 +62,38 @@ public class SubmitterServiceTest extends TestBase {
 		FieldUtils.writeField(submitterService, "cmiiToLmMapper", cmiiToLmMapper, true);
 	}
 
-//	@Test
-//	public void testSubmitterServiceWithSubmissions() throws Exception {
-//
-//		int listSize = 5;
-//		List<CmiiSubmission> cmiiSubmissions = cmiiSubmissionMockerUpper.createList(listSize);
-//		List<JobDto> jobDtos = jobDtoMockerUpper.createList(listSize);
-//		List<Long> jobIds = jobDtos.stream().map(p -> Long.parseLong(p.getJobId())).collect(Collectors.toList());
-//		when(cmiiSubmissionService.getSubmissions(jobIds)).thenReturn(cmiiSubmissions);
-//
-//		when(jobStateManagerAccessor.startNextBatchOfJobs()).thenReturn(jobDtos);
-//
-//		submitterService.submitBatch();
-//
-//		verify(lmSubmissionService, times(listSize)).processSubmission(any(LmSubmission.class));
-//	}
-//
-//	@Test
-//	public void testSubmitterServiceWithNoSubmissions() throws Exception {
-//
-//		int listSize = 0;
-//		List<CmiiSubmission> cmiiSubmissions = cmiiSubmissionMockerUpper.createList(listSize);
-//		List<JobDto> jobDtos = jobDtoMockerUpper.createList(listSize);
-//		List<Long> jobIds = jobDtos.stream().map(p -> Long.parseLong(p.getJobId())).collect(Collectors.toList());
-//		when(cmiiSubmissionService.getSubmissions(jobIds)).thenReturn(cmiiSubmissions);
-//		when(jobStateManagerAccessor.startNextBatchOfJobs()).thenReturn(jobDtos);
-//
-//		submitterService.submitBatch();
-//
-//		verify(lmSubmissionService, times(listSize)).processSubmission(any(LmSubmission.class));
-//	}
-//
+	@Test
+	public void testSubmitterServiceWithSubmissions() throws Exception {
+
+		int listSize = 5;
+		List<CmiiSubmission> cmiiSubmissions = cmiiSubmissionMockerUpper.createList(listSize);
+		List<TaskDto> taskDtos = taskDtoMockerUpper.createList(listSize);
+		List<Long> jobIds = taskDtos.stream().map(p -> Long.parseLong(p.getJobId())).collect(Collectors.toList());
+		when(cmiiSubmissionService.getSubmissions(jobIds)).thenReturn(cmiiSubmissions);
+
+		when(jobStateManagerAccessor.startNextBatchOfTasks()).thenReturn(taskDtos);
+
+		submitterService.submitBatch();
+
+		verify(lmSubmissionService, times(listSize)).processSubmission(any(LmSubmission.class));
+		verify(jobStateManagerAccessor).updateTaskResults((ArgumentMatchers.<TaskDto>anyList()));
+	}
+
+	@Test
+	public void testSubmitterServiceWithNoSubmissions() throws Exception {
+
+		int listSize = 0;
+		List<CmiiSubmission> cmiiSubmissions = cmiiSubmissionMockerUpper.createList(listSize);
+		List<TaskDto> taskDtos = taskDtoMockerUpper.createList(listSize);
+		List<Long> jobIds = taskDtos.stream().map(p -> Long.parseLong(p.getJobId())).collect(Collectors.toList());
+		when(cmiiSubmissionService.getSubmissions(jobIds)).thenReturn(cmiiSubmissions);
+
+		when(jobStateManagerAccessor.startNextBatchOfTasks()).thenReturn(taskDtos);
+
+		submitterService.submitBatch();
+
+		verify(lmSubmissionService, times(listSize)).processSubmission(any(LmSubmission.class));
+		verify(jobStateManagerAccessor, times(0)).updateTaskResults((ArgumentMatchers.<TaskDto>anyList()));
+	}
+
 }
---
+++ src/test/java/gov/nyc/doitt/casematters/submitter/TaskDtoMockerUpper.java
@@ -0,0 +1,41 @@
+package gov.nyc.doitt.casematters.submitter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+import gov.nyc.doitt.casematters.submitter.task.TaskDto;
+import gov.nyc.doitt.casematters.submitter.task.TaskState;
+
+@Component
+public class TaskDtoMockerUpper {
+
+	public String jobId = "1";
+
+	public List<TaskDto> createList(int listSize) throws Exception {
+
+		List<TaskDto> tasks = new ArrayList<>();
+		for (int i = 0; i < listSize; i++) {
+			tasks.add(create(i));
+		}
+		return tasks;
+	}
+
+	public TaskDto create() throws Exception {
+
+		int i = new Random().nextInt(100) * -1;
+		return create(i);
+	}
+
+	private TaskDto create(int idx) throws Exception {
+
+		TaskDto taskDto = new TaskDto();
+		taskDto.setName("nextTaskName" + idx);
+		taskDto.setJobId(jobId);
+		taskDto.setState(TaskState.PROCESSING.toString());
+
+		return taskDto;
+	}
+}
